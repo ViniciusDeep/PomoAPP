b15fc2c1625316135154a03484836c0062705b08
Vinicius Mangueira <Vinicius@imac-de-administrador.local>
2018-05-24 14:38:16 -0300

    Initial Commit

---
+++ Pomodoro Timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pomodoro Timer
+//
+//  Created by Vinicius Mangueira on 24/05/2018.
+//  Copyright © 2018 Vinicius Mangueira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
