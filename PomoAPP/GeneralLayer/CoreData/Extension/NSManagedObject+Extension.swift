932ccc954267ac640b60f110e0490fc9ce88c5bf
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-05-31 09:18:33 -0300

    Implement Core Data with create pomo

---
+++ PomoAPP/GeneralLayer/CoreData/Extension/NSManagedObject+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  NSManagedObject+Extension.swift
+//  PomoAPP
+//
+//  Created by Vinicius Mangueira Correia on 31/05/19.
+//  Copyright © 2019 Vinicius Mangueira Correia. All rights reserved.
+//
+
+import Foundation
